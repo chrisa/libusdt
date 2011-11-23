8c637621bddddbcc7efa6130ac791fe84a629e67
Chris Andrews <chris@nodnol.org>
2011-11-23 08:32:03 +0000

    Create multiple probes in test_usdt

--- test_usdt.c
+++ test_usdt.c
@@ -6,7 +6,7 @@
 
 int main(void) {
   usdt_provider_t provider;
-  usdt_probedef_t probedef;
+  usdt_probedef_t probedef[2];
   char *char_argv[2] = { "foo", "bar" };
 
   provider.name = "testlibusdt";
@@ -14,18 +14,27 @@ int main(void) {
   provider.probes = NULL;
   provider.file = NULL;
   
-  probedef.function = "func";
-  probedef.name = "myprobe";
+  probedef[0].function = "func";
+  probedef[0].name = "probe1";
+  probedef[0].types[0] = USDT_ARGTYPE_STRING;
+  probedef[0].types[1] = USDT_ARGTYPE_STRING;
+  probedef[0].types[2] = USDT_ARGTYPE_NONE;
 
-  probedef.types[0] = USDT_ARGTYPE_STRING;
-  probedef.types[1] = USDT_ARGTYPE_STRING;
-  probedef.types[2] = USDT_ARGTYPE_NONE;
+  probedef[1].function = "func";
+  probedef[1].name = "probe2";
+  probedef[1].types[0] = USDT_ARGTYPE_STRING;
+  probedef[1].types[1] = USDT_ARGTYPE_STRING;
+  probedef[1].types[2] = USDT_ARGTYPE_NONE;
   
-  usdt_provider_add_probe(&provider, &probedef);
+  usdt_provider_add_probe(&provider, &probedef[0]);
+  usdt_provider_add_probe(&provider, &probedef[1]);
   usdt_provider_enable(&provider);
 
-  if (usdt_is_enabled(probedef.probe))
-    usdt_fire_probe(probedef.probe, 2, (void **) &char_argv);
+  if (usdt_is_enabled(probedef[0].probe))
+    usdt_fire_probe(probedef[0].probe, 2, (void **) &char_argv);
+
+  if (usdt_is_enabled(probedef[1].probe))
+    usdt_fire_probe(probedef[1].probe, 2, (void **) &char_argv);
 
   return 0;
 }
