40c64acb5439f408f13a9d6f8a37f7204288067e
Chris Andrews <chris@nodnol.org>
2012-05-01 12:52:50 +0100

    Handle stack arguments properly for x86_64.

--- usdt_tracepoints_x86_64.s
+++ usdt_tracepoints_x86_64.s
@@ -27,7 +27,7 @@ _usdt_tracepoint_isenabled:
         xorq    %rax, %rax
         nop
         nop
-        leave
+        popq    %rbp
         ret
 usdt_tracepoint_probe:
 _usdt_tracepoint_probe:
@@ -36,11 +36,12 @@ _usdt_tracepoint_probe:
         nop
         nop
         nop
-        popq %rbx
-        popq %r13
-        popq %r12
-        addq $0x18,%rsp
-        leave
+        addq	%r14,%rsp
+        popq    %rbx
+        popq    %r14
+        popq    %r13
+        popq    %r12
+        popq    %rbp
 usdt_tracepoint_end:
 _usdt_tracepoint_end:
         ret
@@ -54,15 +55,15 @@ usdt_probe_args:
 _usdt_probe_args:
         pushq   %rbp
         movq    %rsp,%rbp
-        subq    $0x18,%rsp
         pushq   %r12
         pushq   %r13
+        pushq   %r14
         pushq   %rbx
 
         movq    %rdi,%r12
         movq    %rsi,%rbx
         movq    %rdx,%r11
-        movq    $0,%r13
+        movq    $0,%r14
 
         test    %rbx,%rbx
         je      fire
@@ -93,6 +94,21 @@ _usdt_probe_args:
         addq    $8,%r11
         movq    (%r11),%r9
 
+        movq    %rbx,%r13
+morestack:
+        dec     %rbx
+        test    %rbx,%rbx
+        je      args
+        subq    $16,%rsp
+        addq    $16,%r14
+        dec     %rbx
+        test    %rbx,%rbx
+        je      args
+        jmp     morestack
+
+args:
+        movq    %r13,%rbx
+        movq    $0,%r13
 moreargs:
         dec     %rbx
         test    %rbx,%rbx
